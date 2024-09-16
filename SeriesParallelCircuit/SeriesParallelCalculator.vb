36013ac1059ec68b78afe500f03d905f555fbb45
Rahiel Rodriguez <rahielrodriguez@hotmail.com>
2024-09-16 00:35:29 -0600

    Calculations added C, L and F settings added Extra Variables added

--- SeriesParallelCircuit/SeriesParallelCalculator.vb
+++ SeriesParallelCircuit/SeriesParallelCalculator.vb
@@ -1,6 +1,5 @@
 ﻿Option Strict On
 Option Explicit On
-Imports System.Windows.Forms.VisualStyles
 
 
 
@@ -28,9 +27,33 @@ Public Class SeriesParallelCalculator
     Dim r3Value As Double
     Dim l1Value As Double
     Dim rwValue As Double
-    Dim ztotalResult As Double
-    Dim zb1Result As Double
-    Dim zb2Result As Double
+    Dim ztotalreal As Double
+    Dim ztotalimg As Double
+    Dim ztotalpol As Double
+    Dim ztotalangle As Double
+    Dim zseriesreal As Double
+    Dim zseriesimg As Double
+    Dim zseriespol As Double
+    Dim zseriesangle As Double
+    Dim zparallelreal As Double
+    Dim zparallelimg As Double
+    Dim zparallelpol As Double
+    Dim zparallelangle As Double
+    Dim zb1real As Double
+    Dim zb1img As Double
+    Dim zb1pol As Double
+    Dim zb1angle As Double
+    Dim zb2real As Double
+    Dim zb2img As Double
+    Dim zb2pol As Double
+    Dim zb2angle As Double
+    Dim c1valueangle As Double = -90
+    Dim r1valueangle As Double = 0
+    Dim l1valueangle As Double = 90
+    Dim rwvalueangle As Double = 0
+    Dim r2valueangle As Double = 0
+    Dim c2valueangle As Double = -90
+    Dim r3valueangle As Double = 0
     Dim xc1Result As Double
     Dim xc2Result As Double
     Dim xl1Result As Double
@@ -43,6 +66,7 @@ Public Class SeriesParallelCalculator
     Dim il1Result As Double
     Dim ic2Result As Double
     Dim ir3Result As Double
+    Dim vparallel As Double
     Dim vc1Result As Double
     Dim vr1Result As Double
     Dim vl1Result As Double
@@ -54,23 +78,14 @@ Public Class SeriesParallelCalculator
     Sub SetDefaults()
 
         VgenTextBox.Text = ""
-        VgenLabel.Text = ""
         FrequencyTextBox.Text = ""
-        FrequencyLabel.Text = ""
         C1TextBox.Text = ""
-        C1Label.Text = ""
         C2TextBox.Text = ""
-        C2Label.Text = ""
         R1TextBox.Text = ""
-        R1Label.Text = ""
         R2TextBox.Text = ""
-        R2Label.Text = ""
         R3TextBox.Text = ""
-        R3Label.Text = ""
         L1TextBox.Text = ""
-        L1Label.Text = ""
         RwTextBox.Text = ""
-        R2Label.Text = ""
         ResultsListBox.Items.Clear()
 
     End Sub
@@ -104,8 +119,8 @@ Public Class SeriesParallelCalculator
 
             Select Case vgenValue
                 Case <= 0
-                    FrequencyTextBox.BackColor= Color.LightYellow
-                    message &="Valid Frequency Value is Required" & vbNewLine
+                    FrequencyTextBox.BackColor = Color.LightYellow
+                    message &= "Valid Frequency Value is Required" & vbNewLine
                     Return False
                 Case Else
                     FrequencyTextBox.BackColor = Color.White
@@ -266,11 +281,42 @@ Public Class SeriesParallelCalculator
             End Select
 
         Catch ex As Exception
+            rwValue = 0
             RwTextBox.BackColor = Color.White
             Return True
         End Try
 
     End Function
+    Sub cSettings()
+
+        If CmicroRadioButton.Checked = True Then
+            c1Value = CDbl(C1TextBox.Text) * 0.000001
+            c2Value = CDbl(C2TextBox.Text) * 0.000001
+        ElseIf CpicoRadioButton.Checked = True Then
+            c1Value = CDbl(C1TextBox.Text) * 0.000000000001
+            c2Value = CDbl(C2TextBox.Text) * 0.000000000001
+        End If
+
+    End Sub
+    Sub LSettings()
+
+        If LRadioButton.Checked = True Then
+            l1Value = CDbl(L1TextBox.Text)
+        ElseIf LmilliRadioButton.Checked = True Then
+            l1Value = CDbl(L1TextBox.Text) * 0.001
+        ElseIf LmicroRadioButton.Checked = True Then
+            l1Value = CDbl(L1TextBox.Text) * 0.000001
+        End If
+    End Sub
+    Sub FrequencySettings()
+        If HzRadioButton.Checked = True Then
+            frequencyValue = CDbl(frequencyValue)
+        ElseIf kHzRadioButton.Checked = True Then
+            frequencyValue = CDbl(frequencyValue) * 1000
+        ElseIf MHzRadioButton.Checked = True Then
+            frequencyValue = CDbl(frequencyValue) * 1000000
+        End If
+    End Sub
     Function FieldValidation() As Boolean
 
         If VgenValidation() And FrequencyValidation() And C1Validation() And R1Validation() And L1Validation() And RwValidation() And R2Validation() And C2Validation() And R3Validation() = True Then
@@ -279,6 +325,64 @@ Public Class SeriesParallelCalculator
             Return False
         End If
     End Function
+    Sub ReactanceCalculation()
+
+        xc1Result = 1 / (2 * Math.PI * frequencyValue * c1Value)
+
+        xl1Result = 2 * Math.PI * frequencyValue * l1Value
+
+        xc2Result = 1 / (2 * Math.PI * frequencyValue * c2Value)
+
+    End Sub
+    Sub ZCalculations()
+
+        zseriesreal = r1Value
+        zseriesimg = xc1Result
+        zseriespol = Math.Sqrt((zseriesreal ^ 2) + (zseriesimg ^ 2))
+        zseriesangle = Math.Atan(zseriesimg / zseriesreal)
+        zb1real = r2Value + rwValue
+        zb1img = xl1Result
+        zb1pol = Math.Sqrt(((zb1real) ^ 2) + (zb1img ^ 2))
+        zb1angle = Math.Atan(zb1img / zb1real)
+        zb2real = r3Value
+        zb2img = xc2Result
+        zb2pol = Math.Sqrt(((zb2real) ^ 2) + (zb2img ^ 2))
+        zb2angle = Math.Atan(zb2img / zb2real)
+        zparallelpol = (zb1pol * zb2pol) / Math.Atan(zb1img / (Math.Sqrt(((zb1real + zb2real) ^ 2) + (zb1img + zb2img) ^ 2)))
+        zparallelangle = zb1angle + zb2angle - (Math.Atan((zb1img + zb2img) / (zb1real + zb2real)))
+        zparallelreal = zparallelpol * (Math.Cos(zparallelangle))
+        zparallelimg = zparallelpol * (Math.Sin(zparallelangle))
+        ztotalreal = (zseriesreal + zparallelreal)
+        ztotalimg = (zseriesimg + zparallelimg)
+        ztotalpol = Math.Sqrt(((ztotalreal) ^ 2) + (ztotalimg ^ 2))
+        ztotalangle = Math.Atan(ztotalimg / ztotalreal)
+
+    End Sub
+    Sub VandICalculations()
+
+        vc1Result = vgenValue / xc1Result
+
+        vr1Result = vgenValue / r1Value
+
+        itotalResult = vgenValue / ztotalpol
+
+        vparallel = itotalResult * zb1pol
+
+        ib1Result = vparallel / zb1pol
+
+        vl1Result = ib1Result * xl1Result
+
+        vr2Result = ib1Result * r2Value
+
+        ib2Result = vparallel / zb2pol
+
+        vc2Result = ib2Result * xc2Result
+
+        vr3Result = ib2Result * r3Value
+
+    End Sub
+
+
 
     Private Sub CalculateButton_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
         If FieldValidation() = True Then
@@ -287,5 +391,21 @@ Public Class SeriesParallelCalculator
             MsgBox(message, MsgBoxStyle.Critical, "User Information Error")
             message = ""
         End If
+    End Sub
+
+    Private Sub SeriesParallelCalculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        SetDefaults()
+
+    End Sub
+
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        SetDefaults()
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+
     End Sub
 End Class
